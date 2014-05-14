99e7bb9a15003481f1ab8cfd69c74dc1432af8cb
Kazuaki Hamada <kazuaki.hamada@gree.net>
2014-05-14 10:00:36 +0900

    [修正] make-filelist.rbの収集対象外フォルダ

--- .make-filelist.rb
+++ .make-filelist.rb
@@ -2,6 +2,7 @@ $EXCLUDE_PATH = %w[
 . .. backup backup1 backup2 backup-system lost+found tmp temp rubikitch.lost
 .cache w3mtmp proxy-cache trash proc RCS.old
 autom4te.cache blib _build .bzr .cdv cover_db CVS _darcs ~.dep ~.dot .git .hg ~.nib .pc ~.plst RCS SCCS _sgbak .svn
+Library Applications
 ]
 
 # Exclude regexps (backup files, core files, and so on)
@@ -10,11 +11,6 @@ $EXCLUDE_REGEXP = Regexp.union(/~$/, /\#.+\#$/, /[._].*\.swp$/, /core\.\d+$/, # 
 
 # Set default directories to collect
 $LS_DIRS = [
-  "~/emacs/init.d", "~/emacs/lisp", "~/.emacs.d", "~/emacs",
-  "~/gdgd", "~/memo", "~/book", "~/src", "~/public_html", "~/private_html", "~",
-  "/m/usr/share/emacs/23.1.50", "/m/usr/share/emacs/site-lisp", "/m/log/emacswikipages",
-  "/m/home/local/lib/ruby19", "/m/home/local/lib/ruby", "/m/log/compile/ruby19", "/m/log/compile/ruby18",
-  "/m/log/compile", "/m/log", "/m/home/local", "/m/home/nobackup",
-  "/m/usr", "/etc", "/m/home/archives", "/m/l/var",
+  "~",
   "/"
 ]
