a9ea9f5e491bb99995ac04f30729e61a0700f8a6
Kazuaki Hamada <kazuaki.hamada@gree.net>
2014-06-03 17:19:14 +0900

    [add] emacs:tmuxでのクリップボード共有設定

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -374,6 +374,19 @@
 ;; その他設定
 ;;______________________________________________________________________
 
+;; tmux でクリップボード共有
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+
 ;; xterm-mouse-mode
 (unless (fboundp 'track-mouse)
   (defun track-mouse (e)))
