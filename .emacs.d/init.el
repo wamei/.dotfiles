8118e08f4bc0b0fcad094aa90ace9e6b6dbcfdcf
Kazuaki Hamada <kazuaki.hamada@gree.net>
2014-06-18 14:02:09 +0900

    [add] emacs:dired設定

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -384,6 +384,13 @@
 ;; その他設定
 ;;______________________________________________________________________
 
+;; diredを2つのウィンドウで開いている時に、デフォルトの移動orコピー先をもう一方のdiredで開いているディレクトリにする
+(setq dired-dwim-target t)
+;; ディレクトリを再帰的にコピーする
+(setq dired-recursive-copies 'always)
+;; diredバッファでC-sした時にファイル名だけにマッチするように
+(setq dired-isearch-filenames t)
+
 ;; tmux でクリップボード共有
 (defun copy-from-osx ()
   (shell-command-to-string "pbpaste"))
