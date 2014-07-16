4a9b1a99458bb4d9095317d4b29e87c97c6bc296
Kazuaki Hamada <kazuaki.hamada@gree.net>
2014-07-16 11:17:30 +0900

    [add] emacs:magit fullscreen

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -820,6 +820,20 @@ file is a remote file (include directory)."
   (set-face-foreground 'magit-diff-add "#b9ca4a")
   (set-face-foreground 'magit-diff-del "#d54e53")
   (set-face-background 'magit-item-highlight "#000000")
+  (defadvice magit-status (around magit-fullscreen activate)
+    (window-configuration-to-register :magit-fullscreen)
+    ad-do-it
+    (delete-other-windows))
+
+  (defun my/magit-quit-session ()
+    (interactive)
+    (kill-buffer)
+    (jump-to-register :magit-fullscreen))
+
+  (define-key magit-status-mode-map (kbd "q") 'my/magit-quit-session)
+
+  (defadvice git-commit-commit (after move-to-magit-buffer activate)
+    (delete-window))
   )
 
 ;;
