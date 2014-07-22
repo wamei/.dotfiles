078ab1743b7564a32f2d9e138856e5ea6ef26729
Kazuaki Hamada <kazuaki.hamada@gree.net>
2014-07-22 19:39:23 +0900

    [del] emacs:foreign regexp

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1020,15 +1020,6 @@ file is a remote file (include directory)."
   (add-hook 'html-mode-hook 'rainbow-mode)
   (add-hook 'emacs-lisp-mode-hook 'rainbow-mode)
   )
-;;
-;; foreign regexp.el
-;;----------------------------------------------------------------------------------------------------
-(when (require 'foreign-regexp nil t)
-  (custom-set-variables
-   ;; 正規表現、perlかrubyを選択
-   '(foreign-regexp/regexp-type 'perl) ;; Choose by your preference.
-   '(reb-re-syntax 'foreign-regexp)) ;; Tell re-builder to use foreign regexp.
-  )
 
 ;;
 ;; undo-tree.el
