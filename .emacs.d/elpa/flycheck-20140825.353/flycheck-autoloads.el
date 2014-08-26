432f3fa84bdd9dad64212e4c20c463cd7e3943d0
Kazuaki Hamada <kazuaki.hamada@gree.net>
2014-08-26 15:19:15 +0900

    [upd] emacs:packages

--- .emacs.d/elpa/flycheck-20140825.353/flycheck-autoloads.el
+++ .emacs.d/elpa/flycheck-20140825.353/flycheck-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "flycheck" "flycheck.el" (21498 39465 0 0))
+;;;### (autoloads nil "flycheck" "flycheck.el" (21500 9920 0 0))
 ;;; Generated autoloads from flycheck.el
 
 (autoload 'flycheck-mode "flycheck" "\
@@ -134,7 +134,7 @@ The following PROPERTIES constitute an error level:
 
 ;;;***
 
-;;;### (autoloads nil nil ("flycheck-pkg.el") (21498 39465 647649
+;;;### (autoloads nil nil ("flycheck-pkg.el") (21500 9920 621193
 ;;;;;;  0))
 
 ;;;***
--- .emacs.d/elpa/php-mode-20140825.301/php-mode-autoloads.el
+++ .emacs.d/elpa/php-mode-20140825.301/php-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "php-mode" "php-mode.el" (21498 39388 0 0))
+;;;### (autoloads nil "php-mode" "php-mode.el" (21500 9876 0 0))
 ;;; Generated autoloads from php-mode.el
 
 (let ((loads (get 'php 'custom-loads))) (if (member '"php-mode" loads) nil (put 'php 'custom-loads (cons '"php-mode" loads))))
