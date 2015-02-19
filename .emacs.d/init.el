1e478de0cb1697386e1925ed48784ab48107d0bf
Kazuaki Hamada <wamei.cho@gmail.com>
2015-02-19 17:06:27 +0900

    Changed esbl managed by git to package.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -79,7 +79,7 @@
 ;;; package.el
 (when (require 'package nil t)
   ;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
   (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
   )
 
