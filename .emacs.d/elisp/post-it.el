0b3c5eeb1c70812e5407eda1756aa75d435d6538
Kazuaki Hamada <wamei.cho@gmail.com>
2015-03-11 15:39:30 +0900

    Update packages

---
+++ .emacs.d/elisp/post-it.el
@@ -0,0 +1,33 @@
+;;; post-it.el --- Post it
+
+;; Author: wamei <wamei.cho@gmail.com>
+;; Keywords: memo, overlay, post it
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.4") (popup "0.5.2"))
+
+;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Post it.
+;;
+
+;;; Code:
+
+(eval-when-compile (require 'cl))
+(require 'popup)
+
+;;; post-it.el ends here
