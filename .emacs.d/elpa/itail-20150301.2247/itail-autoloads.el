2199a962222b7524e05a1347e614abec09b61c5b
wamei <wamei.cho@gmail.com>
2015-12-03 15:07:07 +0900

    Update packages

--- .emacs.d/elpa/itail-20150301.2247/itail-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; itail-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("itail.el") (21750 29428 335590 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; itail-autoloads.el ends here
--- .emacs.d/elpa/itail-20150301.2247/itail-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "itail" "20150301.2247" "An interactive tail mode" 'nil :url "https://github.com/re5et/itail" :keywords '("tail"))
