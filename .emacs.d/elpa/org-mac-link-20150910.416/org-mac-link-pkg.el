11305062379c70f62e899fdb739f9ae4d273673b
wamei <wamei.cho@gmail.com>
2015-12-22 11:37:06 +0900

    Update packages

--- .emacs.d/elpa/org-mac-link-20150910.416/org-mac-link-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "org-mac-link" "20150910.416" "Insert org-mode links to items selected in various Mac apps" 'nil :keywords '("org" "mac" "hyperlink"))
--- .emacs.d/elpa/web-mode-20151216.1200/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20151216.1200" "major mode for editing web templates" 'nil :url "http://web-mode.org" :keywords '("languages"))
