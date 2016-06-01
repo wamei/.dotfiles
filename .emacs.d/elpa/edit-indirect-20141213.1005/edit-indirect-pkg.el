585dc9527715ed5e6ad0d38f0985ce8795c1e87b
kazuaki hamada <kazuaki.hamada@gree.net>
2016-06-01 13:28:25 +0900

    Update packages

--- .emacs.d/elpa/edit-indirect-20141213.1005/edit-indirect-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "edit-indirect" "20141213.1005" "Edit regions in separate buffers" '((emacs "24.3")) :url "https://github.com/Fanael/edit-indirect")
--- .emacs.d/elpa/orglink-20151106.1006/orglink-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "orglink" "20151106.1006" "use Org Mode links in other modes" '((dash "1.3.2") (org "8.0")) :url "http://github.com/tarsius/orglink" :keywords '("hypertext"))
