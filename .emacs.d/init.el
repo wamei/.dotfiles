f09b0a446387ca19ed0d416a1ddb0ca76867e877
wamei <wamei.cho@gmail.com>
2016-08-08 11:42:17 +0900

    fix package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -29,9 +29,9 @@
 
 ;;; package.el
 (when (require 'package nil t)
-  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+  ;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-  (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
+  ;; (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
   )
 
 (require 'misc)
