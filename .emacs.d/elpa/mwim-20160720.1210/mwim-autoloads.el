800487dada8429ddfe9fa954c047338d668d9466
wamei <wamei.cho@gmail.com>
2016-08-08 11:07:41 +0900

    update packages

---
+++ .emacs.d/elpa/mwim-20160720.1210/mwim-autoloads.el
@@ -0,0 +1,15 @@
+;;; mwim-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("mwim.el") (22439 59583 97159 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; mwim-autoloads.el ends here
