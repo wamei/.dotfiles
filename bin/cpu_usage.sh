b30d8a500248dfc19f4fef177b62049cb0309dc2
kazuaki hamada <kazuaki.hamada@gree.net>
2016-10-07 13:13:41 +0900

    fix usage

--- bin/cpu_usage.sh
+++ bin/cpu_usage.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 cpu_usage="/tmp/cpu.usage"
 if [ "$(uname)" == 'Darwin' ]; then
-    which sar > /dev/null 2>&1 && sar -u 1 | tail -n 1 | awk '{print "CPU:" $5 "%"}'
+    which top > /dev/null 2>&1 && top -R -F -u -l 1 | head -n 4 | tail -n 1 | awk '{print "CPU:" (100.0-substr($7, 1, index($7, "%")-1)) "%"}'
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     [ -f $cpu_usage ] && cat $cpu_usage | awk '{print "CPU:" $1}'
     ~/.dotfiles/bin/cpu_usage_sub.sh > /dev/null 2>&1 &
--- bin/storage_usage.sh
+++ bin/storage_usage.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-which df > /dev/null 2>&1 && df -P | awk 'NR==2 {printf "HDD:%2.1f%%\n", $4/$2*100}'
+which df > /dev/null 2>&1 && df -P | awk 'NR==2 {printf "HDD:%2.1f%%\n", $3/$2*100}'
