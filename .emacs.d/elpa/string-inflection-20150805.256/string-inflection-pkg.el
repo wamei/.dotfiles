94ca176b7ac2da7cedb7e9d15b58cca649b6f23a
wamei <wamei.cho@gmail.com>
2016-12-12 11:32:12 +0900

    update packages

--- .emacs.d/elpa/string-inflection-20150805.256/string-inflection-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "string-inflection" "20150805.256" "underscore -> UPCASE -> CamelCase -> lowerCamelCase conversion of names" 'nil :keywords '("elisp"))
