17a9aae3a2b1a33c1bf27732f70ef26749ea410c
wamei <wamei.cho@gmail.com>
2017-01-17 11:38:36 +0900

    update packages

--- .emacs.d/elpa/company-php-20160910.1747/company-php-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "company-php" "20160910.1747" "company completion source for php" '((cl-lib "0.5") (ac-php-core "1") (company "0.9")) :url "https://github.com/xcwen/ac-php" :keywords '("completion" "convenience" "intellisense"))
---
+++ .emacs.d/elpa/quickrun-20170114.645/quickrun-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "quickrun" "20170114.645" "Run commands quickly" '((emacs "24.3")) :url "https://github.com/syohex/emacs-quickrun")
