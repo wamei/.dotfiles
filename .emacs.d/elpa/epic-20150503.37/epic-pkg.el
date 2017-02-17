5548bfbf4a83080e5e267634ba115af9148b14fc
kazuaki hamada <kazuaki.hamada@gree.net>
2017-02-17 15:49:27 +0900

    update packages

--- .emacs.d/elpa/epic-20150503.37/epic-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "epic" "20150503.37" "Evernote Picker for Cocoa Emacs" '((htmlize "1.47")) :url "https://github.com/yoshinari-nomura/epic" :keywords '("evernote" "applescript"))
---
+++ .emacs.d/elpa/pcmpl-git-20170120.1659/pcmpl-git-autoloads.el
@@ -0,0 +1,27 @@
+;;; pcmpl-git-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "pcmpl-git" "pcmpl-git.el" (22694 40102 516111
+;;;;;;  973000))
+;;; Generated autoloads from pcmpl-git.el
+
+(autoload 'pcomplete/git "pcmpl-git" "\
+Completion rules for the `git' command.
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("pcmpl-git-parse.el" "pcmpl-git-pkg.el")
+;;;;;;  (22694 40102 524111 993000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pcmpl-git-autoloads.el ends here
