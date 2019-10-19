95f91c693f2d20aa085ca6f828bf331d9b3c7aa0
wamei <wamei.cho@gmail.com>
2019-10-20 03:43:17 +0900

    update packages

---
+++ .emacs.d/elpa/math-symbol-lists-20190605.2058/math-symbol-lists-autoloads.el
@@ -0,0 +1,40 @@
+;;; math-symbol-lists-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads "actual autoloads are elsewhere" "math-symbol-lists"
+;;;;;;  "../../../../.emacs.d/elpa/math-symbol-lists-20190605.2058/math-symbol-lists.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../.emacs.d/elpa/math-symbol-lists-20190605.2058/math-symbol-lists.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "math-symbol-lists" '("math-symbol-list-")))
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "msl-build" "../../../../.emacs.d/elpa/math-symbol-lists-20190605.2058/msl-build.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../.emacs.d/elpa/math-symbol-lists-20190605.2058/msl-build.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "msl-build" '("msl-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/math-symbol-lists-20190605.2058/math-symbol-lists-autoloads.el"
+;;;;;;  "../../../../.emacs.d/elpa/math-symbol-lists-20190605.2058/math-symbol-lists-pkg.el"
+;;;;;;  "../../../../.emacs.d/elpa/math-symbol-lists-20190605.2058/math-symbol-lists.el"
+;;;;;;  "../../../../.emacs.d/elpa/math-symbol-lists-20190605.2058/msl-build.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; math-symbol-lists-autoloads.el ends here
--- .emacs.d/elpa/orglink-20180318.2023/orglink-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "orglink" "20180318.2023" "use Org Mode links in other modes" '((emacs "24.3") (dash "2.12.1") (org "8.3")) :commit "e9e90e16ddaceaf99c9b251a215d6338b9762b4d" :keywords '("hypertext") :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/tarsius/orglink")
