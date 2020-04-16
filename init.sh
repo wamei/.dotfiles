09875d9aed5f0e2b78f1b62f3b38cdc297ecabaf
Kazuaki Hamada <kazuaki.hamada@g-pc-00534459.intra.gree-office.net>
2020-04-17 00:46:01 +0900

    update misc

--- init.sh
+++ init.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
+
+# make symbolic links
 ln -sf ~/.dotfiles/.inputrc ~/.inputrc
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 #ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -sfn ~/.dotfiles/.emacs.d/ ~/.emacs.d
 
+# copy bin
+mkdir -p ~/bin
+cp -f ~/.dotfiles/bin/rpbcopy ~/bin
+
+# setting git config
 git config --global color.ui auto
 git config --global core.excludesfile "~/.gitignore"
