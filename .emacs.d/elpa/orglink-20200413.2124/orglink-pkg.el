a677df4bc14589c8d97ef94725a291e23ae60a98
Kazuaki Hamada <kazuaki.hamada@g-pc-00534459.intra.gree-office.net>
2020-04-17 16:22:33 +0900

    update packages

---
+++ .emacs.d/elpa/orglink-20200413.2124/orglink-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "orglink" "20200413.2124" "use Org Mode links in other modes" '((emacs "24.3") (dash "2.12.1") (org "9.3")) :commit "a5f5da06174d22dc21bbac167a8c2361fa84bf2b" :keywords '("hypertext") :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/tarsius/orglink")
---
+++ .emacs.d/elpa/orglue-20200411.311/orglue-pkg.el
@@ -0,0 +1,12 @@
+(define-package "orglue" "20200411.311" "more functionality to org-mode."
+  '((org "9.3")
+    (epic "0.2"))
+  :keywords
+  '("org")
+  :authors
+  '(("Yoshinari Nomura" . "nom@quickhack.net"))
+  :maintainer
+  '("Yoshinari Nomura" . "nom@quickhack.net"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
