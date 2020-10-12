618bf0550edc9ce407bb17eb82f2e612a45e4eac
wamei <wamei.cho@gmail.com>
2020-10-12 12:24:03 +0900

    設定更新

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -92,6 +92,9 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 
+;; フレームのリサイズ
+(setq frame-resize-pixelwise t)
+
 ;; symbolic link のファイル名のまま開く
 (setq find-file-visit-truename nil)
 
@@ -138,11 +141,7 @@
 (transient-mark-mode t)
 
 ;; フレームサイズ
-(setq default-frame-alist
-  '(
-    (width . 240)
-    (height . 70)
-    ))
+(toggle-frame-maximized)
 
 ;; 行番号を表示する
 (global-display-line-numbers-mode)
@@ -976,7 +975,11 @@
     :custom
     (lsp-ui-doc-header t)
     (lsp-ui-doc-include-signature t)
-    (lsp-ui-doc-position 'top)
+    (lsp-ui-doc-position 'at-point)
+    (lsp-ui-doc-max-width 150)
+    (lsp-ui-doc-max-height 30)
+    (lsp-ui-doc-use-childframe t)
+    (lsp-ui-doc-use-webkit t)
     :bind (("<f10>"   . lsp-ui-imenu)
            :map lsp-mode-map
            ("M-s r" . lsp-ui-peek-find-references)
